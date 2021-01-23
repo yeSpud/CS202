0b2aa130cd1346a318ec78754b05576da56290fc
Spud <sogden3@alaska.edu>
2021-01-22 17:12:25 -0900

    Fixed documentaiton and cleaned up excerpt.cpp

--- HW0/excerpt.cpp
+++ HW0/excerpt.cpp
@@ -7,10 +7,17 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 #include <fstream>
 
-// Function used to parse the entries in the book.
-std::vector<std::string> parseBook(std::ifstream& fileStream) {
+/**
+ * Function used to parse the entries in the book.
+ * @param fileStream The file stream (think file object).
+ * If there is an issue with the fileStream the function will return an emtpy vector.
+ * @return Returns a vector containing all the passages from the book (seperated by new lines).
+ * If there was an error with the fileStream this will return an empty vector instead.
+ */
+std::vector<std::string> parseBook(std::ifstream &fileStream) {
 
 	// Create a vector to store all the entries.
 	std::vector<std::string> entries;
@@ -29,8 +36,8 @@ std::vector<std::string> parseBook(std::ifstream& fileStream) {
 	while (std::getline(fileStream, bufferString)) {
 
 		// Make sure we arent adding newlines or nulls.
-		if (bufferString != "\n" && bufferString != "\000") {
-			//std::cout << "Adding: " << bufferString << std::endl;
+		if (bufferString != "\n" && bufferString != "\0") {
+
 			// Append the string to the entries vector
 			entries.push_back(bufferString);
 		}
@@ -41,13 +48,12 @@ std::vector<std::string> parseBook(std::ifstream& fileStream) {
 
 int main(int argc, char *argv[]) {
 
-	// Check for valid arguments
+	// Check for valid arguments\
+	// This tends to be picky so be sure to use the full file path if you can.
 	if (argc != 2) {
 		std::cout << "Usage: ./excerpt <file>" << std::endl;
 		return -1;
 	}
-
-	std::cout << "Reading from file " << argv[1] << std::endl;
 
 	// Create a filestream based on the argument given
 	std::ifstream file(argv[1]);
@@ -56,11 +62,14 @@ int main(int argc, char *argv[]) {
 	std::vector<std::string> entries = parseBook(file);
 	file.close();
 
-	// Get an entry index dependent on the array size. This doesnt have to be truly random.
+	// Get an entry index dependent on the array size.
 	// This will get an int between 0 and the size of the entries vector.
-	int excerptIndex = std::rand() % entries.size();
+	std::random_device randomDevice;
+	std::mt19937 randomGenerator(randomDevice());
+	std::uniform_int_distribution<int> distribution(0, entries.size());
+	int excerptIndex = distribution(randomGenerator);
 
-	std::cout << entries.at(excerptIndex) << std::endl;
+	std::cout << "Excerpt from line " << excerptIndex << ":\""<< entries.at(excerptIndex) << "\"" << std::endl;
 
 	return 0;
 }
