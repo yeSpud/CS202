385b74eaf70b3c77f18613aecfc10e1081310fa7
Spud <sogden3@alaska.edu>
2021-01-22 15:22:10 -0900

    Started work on temperature

---
+++ HW0/temperature.cpp
@@ -0,0 +1,18 @@
+/**
+ * temperature.cpp
+ * Created by Stephen on 1/22/2021.
+ * Converts temperature from degrees Fahrenheit to degrees Celsius.
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    // Check for correct program arguments
+    if (argc != 3) {
+    	std::cout << "Usage:./temperature --<ctof | ftoc> <temperature>" << std::endl;
+	    return -1;
+    }
+
+	// TODO
+}
