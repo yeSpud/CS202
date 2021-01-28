1c53440e90097c683b1fbd6bb52c715873f3d348
Spud <sogden3@alaska.edu>
2021-01-28 10:04:15 -0900

    Added outputs and raw pointer

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "MyClass.hpp"
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	std::cout << "Creating raw pointer dynamically" << std::endl;
+	MyClass* myClassPointer;
+	myClassPointer = new MyClass("Raw pointer class");
+	// Seems that this object is never destroyed, so well take care of that
+	delete myClassPointer;
+
+	std::cout << "Creating unique pointer" << std::endl;
+	// TODO
+
+	std::cout << "Transferring ownership of unique pointer" << std::endl;
+	// TODO
+
+	std::cout << "Calling function within class" << std::endl;
+	// TODO
+
+	std::cout << "Creating shared pointer" << std::endl;
+	// TODO
+
+	std::cout << "Creating another shared pointer based off of the first one" << std::endl;
+	// TODO
+
 	return 0;
 }
