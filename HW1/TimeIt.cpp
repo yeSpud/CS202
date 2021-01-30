6155cfc86c1341678869ab2075d9beaf0629bb70
Spud <sogden3@alaska.edu>
2021-01-29 16:45:58 -0900

    Added TimeIt main file

---
+++ HW1/TimeIt.cpp
@@ -0,0 +1,14 @@
+/**
+ * TimeIt.cpp
+ * Created by Stephen on 1/29/2021.
+ * Whats my purpose?
+ */
+
+#include <iostream>
+#include "StopWatch.hpp"
+
+int main(int argc, char *argv[]) {
+	// TODO
+
+	return 0;
+}
