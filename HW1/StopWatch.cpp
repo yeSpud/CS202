7d81423f3e0771636894e83dd52ada20c4be67f0
Spud <sogden3@alaska.edu>
2021-01-29 16:41:30 -0900

    Added StopWatch.cpp

---
+++ HW1/StopWatch.cpp
@@ -0,0 +1,6 @@
+/**
+ * StopWatch.cpp
+ * Created by Stephen on 1/29/2021.
+ * Whats my purpose?
+ */
+#include "StopWatch.hpp"
---
+++ HW1/StopWatch.hpp
@@ -0,0 +1,16 @@
+/**
+ * StopWatch.hpp
+ * Created by Stephen on 1/29/2021.
+ * Whats my purpose?
+ */
+
+#ifndef HW1_STOPWATCH_HPP
+#define HW1_STOPWATCH_HPP
+
+
+class StopWatch {
+
+};
+
+
+#endif //HW1_STOPWATCH_HPP
