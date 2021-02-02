7b3a7a0279ac4c36fcf70484053b2c6e5cb363c7
Spud <sogden3@alaska.edu>
2021-02-01 21:56:00 -0900

    Added documentaiton to StopWatch.hpp

--- HW1/StopWatch.cpp
+++ HW1/StopWatch.cpp
@@ -1,6 +1,30 @@
 /**
  * StopWatch.cpp
  * Created by Stephen on 1/29/2021.
- * Whats my purpose?
+ * Class that tracks elapsed time. See StopWatch.hpp for function documentation.
  */
+
 #include "StopWatch.hpp"
+#include <chrono>
+
+StopWatch::StopWatch() {
+	// TODO
+}
+
+void StopWatch::Start() {
+	// TODO
+}
+
+void StopWatch::Stop() {
+	// TODO
+}
+
+double StopWatch::getSeconds() {
+	// TODO
+	return 0.0;
+}
+
+double StopWatch::getMilliseconds() {
+	// TODO
+	return 0.0;
+}
--- HW1/StopWatch.hpp
+++ HW1/StopWatch.hpp
@@ -1,15 +1,45 @@
 /**
  * StopWatch.hpp
  * Created by Stephen on 1/29/2021.
- * Whats my purpose?
+ * Class that tracks elapsed time.
  */
 
 #ifndef HW1_STOPWATCH_HPP
 #define HW1_STOPWATCH_HPP
 
+#include <chrono>
 
 class StopWatch {
 
+private:
+
+	std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+
+public:
+
+	StopWatch();
+
+	/**
+	 * (Re)starts the clock.
+	 */
+	void Start();
+
+	/**
+	 * Stops the clock.
+	 */
+	void Stop();
+
+	/**
+	 * Gets the elapsed time in seconds.
+	 * @return The elapsed time in seconds.
+	 */
+	double getSeconds();
+
+	/**
+	 * Gets the elapsed time in milliseconds
+	 * @return The elapsed time in milliseconds.
+	 */
+	double getMilliseconds();
 };
 
 
