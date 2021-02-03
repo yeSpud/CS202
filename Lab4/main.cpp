adc7955c969cd2535bcd7d1c848c7fd8b3ab91ba
Spud <sogden3@alaska.edu>
2021-02-02 15:29:54 -0900

    Got inputs working

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -6,9 +6,11 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 void readFromFile(const std::string& filePath);
+void getInputs(int& n, std::string& str);
 void appendToFile(const std::string& filePath, const int n, const std::string str);
 
 int main(int argc, char *argv[]) {
@@ -21,7 +23,15 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Read the contents of the file. This function should output to the console.
-	readFromFile(argv[1]);
+	//readFromFile(argv[1]);
+
+	// Get our inputs.
+	int n;
+	std::string str;
+	getInputs(n, str);
+
+	// Append the string to the file n number of times.
+	//appendToFile(argv[1], n, str);
 
 	return 0;
 }
@@ -51,6 +61,28 @@ void readFromFile(const std::string& filePath) {
 	}
 
 	// We could close the file here, but the destructor does that for us.
+}
+
+void getInputs(int& n, std::string& str) {
+
+	// Get a number and string from the user (all on one line).
+	std::cout << "Gimme number and a line of text: ";
+
+	// Store both the input number and text all in one string for future processing.
+	std::string rawInput;
+	std::getline(std::cin, rawInput);
+
+	// String stream for parsing
+	std::istringstream istring(rawInput);
+
+	// Get our number from our stream.
+	istring >> n;
+
+	// Get our string from our stream.
+	std::getline(istring, str);
+
+	// Replace the first space left over from parsing.
+	str.replace(0, 1, "");
 }
 
 void appendToFile(const std::string& filePath, const int n, const std::string str) {
