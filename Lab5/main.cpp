414edac41b5e3de6bafe803276034a4f075f6ebf
Spud <sogden3@alaska.edu>
2021-02-04 10:58:03 -0900

    Added functionality

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	// Open the file using ifstream in bin mode.
+	std::ifstream infile(R"(C:\Users\Stephen\Programs\CS202\Lab5\data.dat)", std::ios::binary);
+
+	// Check that it was opened
+	if (!infile) {
+		std::cout << "Unable to open file" << std::endl;
+		return -1;
+	}
+
+	// Get all the numbers from the file
+	int n;
+	std::vector<int> inputs;
+	while (!infile.eof()) {
+		infile.read((char *) &n, sizeof(int));
+		inputs.push_back(n);
+		//std::cout << n << std::endl;
+	}
+
+	std::cout << "Number of integers: " << inputs.size() << std::endl;
+
+	// Get the sum
+	int sum = 0;
+	for (int v : inputs) {
+		sum+=v;
+	}
+
+	// Print the sum and average
+	std::cout << "Sum: " << sum << std::endl;
+	std::cout << "Average: " << (sum/inputs.size()) << std::endl;
+
 	return 0;
 }
