4be9a8a33f475c4a9434663b8f1da06aa9e36223
Spud <sogden3@alaska.edu>
2021-02-12 18:36:07 -0900

    Started implementing LinkedList.cpp

--- HW2/LinkedList.cpp
+++ HW2/LinkedList.cpp
@@ -1,13 +1,44 @@
 /**
  * LinkedList.cpp
  * Created by Stephen on 2/12/2021.
- * Whats my purpose?
+ * Uses catch 2 to test various functions for a queue, stack, and linked list.
  */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-	// TODO
+// Create a struct object with 6 parameters for testing.
+struct Value {
+	int number;
+	double decimal;
+	char character;
+	std::string name;
+	int number2;
+	std::string description;
+};
 
-	return 0;
+TEST_CASE("Queue test") {
+	// TODO create test values
+
+	// TODO Create queue to store test values
+
+	// TODO Test queue
+}
+
+TEST_CASE("Stack test") {
+	// TODO create test values
+
+	// TODO Create stack to store test values
+
+	// TODO Test stack
+}
+
+TEST_CASE("Linked list test") {
+	// TODO create test values
+
+	// TODO Create linked list to store test values
+
+	// TODO Test linked list
 }
