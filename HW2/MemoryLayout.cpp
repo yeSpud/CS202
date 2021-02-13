fc595dfbf95398fc4acbcd71aae362f6bd5e5c22
Spud <sogden3@alaska.edu>
2021-02-12 20:52:48 -0900

    added printPointer function from pdf

--- HW2/MemoryLayout.cpp
+++ HW2/MemoryLayout.cpp
@@ -1,10 +1,18 @@
 /**
  * MemoryLayout.cpp
  * Created by Stephen on 2/12/2021.
- * Whats my purpose?
+ * Tells the order of static storage, the stack, and free storage are layed out in memory.
  */
 
 #include <iostream>
+
+void printPointer(int i) {
+	int* iptr = &i;
+
+	// the unary * operator dereferences the pointer
+	std::cout << "i  =" << *iptr << "\n";
+	std::cout << "&i = " << iptr << "\n";
+}
 
 int main(int argc, char *argv[]) {
 	// TODO
