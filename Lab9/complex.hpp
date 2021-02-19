e4b678e35549a392ee4a15b976371321fea1b746
Spud <sogden3@alaska.edu>
2021-02-18 23:20:22 -0900

    added complex files

---
+++ Lab9/complex.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Stephen on 2/18/2021.
+//
+
+#ifndef LAB9_COMPLEX_HPP
+#define LAB9_COMPLEX_HPP
+
+#include <iostream>
+
+class complex {
+
+public:
+
+	double real, imaginary;
+
+	complex(double real, double i);
+
+	explicit complex(double real);
+
+	friend std::ostream& operator<<(std::ostream &os, const complex &c);
+
+	friend complex operator+(const complex &c0, const complex &c1);
+
+	friend complex operator+=(complex &c0, const complex &c1);
+
+};
+
+
+#endif //LAB9_COMPLEX_HPP
