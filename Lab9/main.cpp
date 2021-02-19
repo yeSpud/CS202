c4978a62f13d6c1d6abd0711690c4afff693b30c
Spud <sogden3@alaska.edu>
2021-02-18 23:32:12 -0900

    Added test for +

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -4,11 +4,16 @@
 int main() {
 
 	// Create a test complex numbers.
-	complex complex1 = complex(1.0, 1.0);
+	complex complex1 = complex(1.1, 1.1);
 	complex complex2 = complex(2.2, 2.2);
 
 	// Test the output stream.
 	std::cout << "Complex 1: " << complex1 << std::endl;
 	std::cout << "Complex 2: " << complex2 << std::endl;
 
+	// Test the + operator.
+	complex complex3 = complex1 + complex2;
+	std::cout << "\nComplex 1: " << complex1 << std::endl;
+	std::cout << "Complex 2: " << complex2 << std::endl;
+	std::cout << "Complex 3: " << complex3 << std::endl;
 }
