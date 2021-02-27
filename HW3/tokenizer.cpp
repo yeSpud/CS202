c45ed10909286d548b5172434bd5ef8996cc28e9
Spud <sogden3@alaska.edu>
2021-02-26 19:30:14 -0900

    Added rough outline of what is needed for tokenizer

--- HW3/tokenizer.cpp
+++ HW3/tokenizer.cpp
@@ -1,10 +1,25 @@
 /**
  * tokenizer.cpp
  * Created by Stephen on 2/26/2021.
- * Whats my purpose?
+ * Takes an input (file or standard input which is specified as a command line argument),
+ * and prints the tokens along with the line and column each token occurs at.
  */
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+// Declare a struct that we will use
+struct TokenAndPosition {
+	std::string _token;
+	int _line;
+	unsigned int _column;
+};
+
+// Declare methods to be implemented later
+std::vector<std::string> lineToToken(const std::string &line);
+std::vector<TokenAndPosition> readLines(std::istream &is);
+void printTokens(std::ostream &os, const std::vector<TokenAndPosition> &tokens);
 
 int main(int argc, char *argv[]) {
 	// TODO
