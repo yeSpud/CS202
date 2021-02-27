c80da860ed254b0bb1ed344429469fe835f68d8b
Spud <sogden3@alaska.edu>
2021-02-26 19:59:17 -0900

    Started implementing boolean operators

--- HW3/Money.cpp
+++ HW3/Money.cpp
@@ -5,3 +5,37 @@
  */
 
 #include "Money.hpp"
+
+Money::Money():dollarValue(0.0) {}
+
+Money::Money(float amount):dollarValue(amount) {}
+
+std::ostream& operator<<(std::ostream &os, const Money &m) {
+	// TODO
+
+	return os;
+}
+
+bool operator==(const Money &m1, const Money& m2) {
+	return m1.dollarValue == m2.dollarValue;
+}
+
+bool operator != (const Money &m1, const Money& m2) {
+	return m1.dollarValue != m2.dollarValue;
+}
+
+bool operator <= (const Money &m1, const Money& m2) {
+	return m1.dollarValue <= m2.dollarValue;
+}
+
+bool operator < (const Money &m1, const Money& m2) {
+	return m1.dollarValue < m2.dollarValue;
+}
+
+bool operator > (const Money &m1, const Money& m2) {
+	return m1.dollarValue > m2.dollarValue;
+}
+
+bool operator >= (const Money &m1, const Money& m2) {
+	return m1.dollarValue >= m2.dollarValue;
+}
