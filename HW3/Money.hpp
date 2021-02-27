c6f84656b8a7a490e446cbc7003304ef32dc3f1f
Spud <sogden3@alaska.edu>
2021-02-26 19:58:59 -0900

    Added operators

--- HW3/Money.hpp
+++ HW3/Money.hpp
@@ -7,8 +7,49 @@
 #ifndef HW3_MONEY_HPP
 #define HW3_MONEY_HPP
 
+#include <iostream>
 
 class Money {
+
+private:
+
+	float dollarValue = 0.0;
+
+public:
+
+	Money();
+
+	explicit Money(float amount);
+
+	friend std::ostream& operator<<(std::ostream &os, const Money &m);
+
+	friend bool operator==(const Money &m1, const Money& m2);
+
+	friend bool operator!=(const Money &m1, const Money& m2);
+
+	friend bool operator<=(const Money &m1, const Money& m2);
+
+	friend bool operator<(const Money &m1, const Money& m2);
+
+	friend bool operator>(const Money &m1, const Money& m2);
+
+	friend bool operator>=(const Money &m1, const Money& m2);
+
+	friend Money& operator-=(const Money &m1, const Money& m2);
+
+	friend Money& operator+=(const Money &m1, const Money& m2);
+
+	friend Money& operator/=(const Money &m1, const Money& m2);
+
+	friend Money& operator*=(const Money &m1, const Money& m2);
+
+	friend Money operator+(const Money &m1, const Money& m2);
+
+	friend Money operator-(const Money &m1, const Money& m2);
+
+	friend Money operator*(const Money &m1, const Money& m2);
+
+	friend Money operator/(const Money &m1, const Money& m2);
 
 };
 
