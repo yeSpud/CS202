5e311f127c5ff092b796d178aae103d6dc6e378b
Spud <sogden3@alaska.edu>
2021-02-26 23:40:52 -0900

    Added functions

--- HW3/tokenizer.cpp
+++ HW3/tokenizer.cpp
@@ -8,6 +8,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iomanip>
 
 // Declare a struct that we will use
 struct TokenAndPosition {
@@ -22,7 +26,77 @@ std::vector<TokenAndPosition> readLines(std::istream &is);
 void printTokens(std::ostream &os, const std::vector<TokenAndPosition> &tokens);
 
 int main(int argc, char *argv[]) {
-	// TODO
+
+	// Check that we have at least a file.
+	if (argc < 2) {
+		std::cout << "Usage: ./tokenizer <file>" << std::endl;
+		return -1;
+	}
+
+	// Try reading the file.
+	std::ifstream file;
+	file.open(argv[1]);
+	if (file.fail()) {
+		std::cout << "Unable to open file." << std::endl;
+		return -2;
+	}
+
+	// Get our tokens
+	std::vector<TokenAndPosition> tokens = readLines(file);
+
+	// Print our tokens.
+	printTokens(std::cout, tokens);
 
 	return 0;
+}
+
+std::vector<std::string> lineToToken(const std::string &line) {
+
+	// Create a string stream.
+	std::istringstream iss(line);
+
+	// Create an array to return, and fill it with our strings.
+	std::vector<std::string> strings((std::istream_iterator<std::string>(iss)),std::istream_iterator<std::string>());
+
+	// Return our string.
+	return strings;
+
+}
+
+std::vector<TokenAndPosition> readLines(std::istream &is) {
+
+	// Create a vector to return.
+	std::vector<TokenAndPosition> tokens;
+
+	// Read from the file line by line and convert the string to a token.
+	std::string bufferString;
+	int lineNumber = 0;
+	while (std::getline(is, bufferString)) {
+		// Start by getting each string in the line, and iterate over them.
+		int columnNumber = 0;
+		for (const std::string& str: lineToToken(bufferString)) {
+			// Convert that line to a token
+			TokenAndPosition token;
+			token._token = str;
+			token._column = columnNumber;
+			token._line = lineNumber;
+
+			// Add the token to the array.
+			tokens.push_back(token);
+			columnNumber++;
+		}
+
+		lineNumber++;
+	}
+
+	return tokens;
+}
+
+void printTokens(std::ostream &os, const std::vector<TokenAndPosition> &tokens) {
+
+	// Iterate though each token.
+	for (TokenAndPosition token : tokens) {
+		// Print our token to the specified output stream.
+		os << "Line" << std::setw(5) << token._line << ", Column" << std::setw(7) << token._column << ": " << std::quoted(token._token) << std::endl;
+	}
 }
